aa48d0e389f773d40c04358645655d63e60263c3
veyorokon <veyor@DESKTOP-93D0BKC>
2016-12-06 23:32:55 -0500

    Added print to file.

--- Project4_Eyorokon/HashTable.h
+++ Project4_Eyorokon/HashTable.h
@@ -1,23 +1,27 @@
 #pragma once
-#include <vector>
 #include <iostream>
 #include <string>
+#include "Record.h"
+#include <iomanip>
+#include <float.h>
+#include <fstream>
+
+#define MAXHASH 1000
+#define HASHPRIME 769
 
 using namespace std;
 
 template <class T> class HashTable {
-#define MAXHASH 1000
-#define MAXPRIME 997	
-public:
-	vector<vector<int>> table; // Stores our data :[0] key, [1] collisions
-	vector<T> data; //Stores our data
-	int size;
 
+
+public:
+	Record<T>* table;
+	int size;
 	/*Default constructor for the hash table.*/
 	HashTable<T>::HashTable()
 	{
-		table.resize(MAXHASH, vector<int>(2, NULL));
-		data.resize(MAXHASH, NULL);
+		table = new Record<T>[MAXHASH];
+		//create(0, 12, 9999);
 		size = 0;
 	}
 
@@ -27,38 +31,99 @@ public:
 
 
 	/*INSERT OPERATOR OVERLOADING HERE!!!!!!*/
-
+	friend ostream& operator<<(ostream& os, HashTable& me) {
+		for (int i = 0; i < MAXHASH; i++) 
+			os << "Index: "<< i << ". " << me.table[i] << endl;
+		return os;
+	}
 
 	/* Inserts the key into the table. If the key is a duplicate, then the function
 	returns false. Else the function returns true.*/
 	bool HashTable<T>::insert(int key, T value, int& collisions) {
-		int home = hash(key), index;
-		if (data[home] == NULL) {
-			assign(home, key, value, 0);
-			return true;
+		int collides = 0;
+		int index = (key*key) % MAXHASH; //Quadratic hash
+		collisions = collides;
+		if (table[index].isEmpty()) return create(index, key, value, collisions); //If index is empty - create new record.
+		collides++; //Increment collision count
+		index = probe(index, key); //Else double hash for new index
+		while (!table[index].isEmpty()) {
+			if (table[index].getKey() == key) {//Does that index have an equal key?
+				if (!table[index].isTombstone()) {//If so, is it a tombstone?
+					cout << "Duplicate.";
+					collisions = collides;//If it isn't a tombstone, return false;
+					return 0;
+				}
+				return create(index, key, value, collisions);//If not, create the node if the index was removed previously.
+			}
+			index++;//Linear probe from second index
+			index %= MAXHASH;//Bound check
+			collides++; //update collisions
 		}
-		collisions += 1;
-		index = probe(home, key, collisions);
-		if (index == -1) return false;
-		assign(index, key, value, collisions);
-		return true;
+		collisions = collides;
+		cout << index << ". Collisions: "<< collisions << ". Expected: "<< expectedCollisions();
+		
+		return create(index, key, value, collides);//create the node
 	}
 
 	/*Removes the key from the table. Returns true if the key was successfully removed. Otherwise,
 	the function returns false. */
 	bool HashTable<T>::remove(int key) {
-
+		int index = hash(key), home = index; //Declare vars
+		if (table[index].getKey() == key) {
+			table[index].kill();//kill index that has key.
+			size--;
+			return true;//check if first hash index has key
+		}
+		index = probe(index, key);//get probe index 
+		while (index != home) {
+			if (table[index].getKey() == key) {
+				table[index].kill();//delete the index if it has the key
+				size--;
+				return true;//return true if probe index has key
+			}
+			index++;
+			index %= MAXHASH;
+		}
+		return false;
 	}
 
 	/*Searches the table for the key, returns true if the key is found. Else, the function returns
 	false. */
 	bool HashTable<T>::find(int key, T& value) {
-
+		int index = hash(key), home = index; //Declare vars
+		if (table[index].getKey() == key && !table[index].isTombstone()) {
+			value = table[index].getValue();//save return value
+			return true;//check if first hash index has key
+		}
+		index = probe(index, key);//get probe index 
+		while (index != home) {
+			if (table[index].getKey() == key && !table[index].isTombstone()) {
+				value = table[index].getValue();//save return value
+				return true;//return true if probe index has key
+			}
+			index++;
+			index %= MAXHASH;
+		}
+		cout << "Not found" << endl;
+		return false;
 	}
 
 	/*Returns the loading factor of the table.*/
 	float HashTable<T>::alpha() {
+		float a = (float(size) / MAXHASH);
+		return a;
+	}
 
+	/*Returns the loading factor of the table.*/
+	float HashTable<T>::expectedCollisions() {
+		float a = alpha();
+		float collisions = float(1 / (1 - a));
+		collisions = floor(collisions + 0.5);
+		ofstream out;
+		out.open("output.txt", fstream::app);
+		out << a << "  " << collisions <<"\n";
+		out.close();
+		return collisions;
 	}
 
 	/*Quadratic hash returns the row index modded by MAXHASH to insert key.*/
@@ -66,34 +131,25 @@ public:
 		return(key * key) % MAXHASH;
 	}
 
-	/*Probes the table for the next available slot. Returns the first index found. */
-	int HashTable<T>::probe(int home, int key, int &collisions) {
-		int jump = 0, index = (jump+home)%MAXHASH;
-		bool duplicate = isDuplicate(key, home);
-		//Some hash function on index. - While - check duplicates (return -1)
-		while (data[index] != NULL) { //Loop until we find an open spot
-			if (duplicate) return -1; //if duplicate return 
-			jump = (MAXPRIME-(key % MAXPRIME)); //update the jump value using Double hash
-			cout << "Key: " << key << " jumped by: " << jump << endl;
-			index = (home + jump) % MAXHASH; //update the index to equal jump value + home
-			duplicate = isDuplicate(key, index); //Check for duplicates
-			collisions += 1;
-		}
-		return index; //return index
-	}
-
-	/*Checks if the key at the index is equal to a given key and returns approprate bool value.*/
-	bool HashTable<T>::isDuplicate(int key, int index) {
-		if (table[index][0] == key) return true;
-		return false;
+	/*Probes the table for the next available slot using double hashing. Returns the first index found. Double
+	hashing algorithm was reviewed at: http://faculty.cs.niu.edu/~freedman/340/340notes/340hash.htm*/
+	int HashTable<T>::probe(int home, int key) {
+		int jump = (HASHPRIME - (key % HASHPRIME)),
+			index = (jump+home)%MAXHASH;
+		return index;
 	}
 
 	/*Assign key and value at index(row) in both table and data vectors.*/
-	void HashTable<T>::assign(int index, int key, T value, int collisions) {
-		data[index] = value;
-		table[index][0] = key;
-		table[index][1] = collisions;
+	bool HashTable<T>::create(int index, int key, T value, int collisions) {
+		table[index] = Record<T>(key, value);
+
+		ofstream out;
+		out.open("output2.txt", fstream::app);
+		out << alpha() << "  " << collisions << "\n";
+		out.close();
+
 		size++; 
+		return true;
 	}
 };
 
--- Project4_Eyorokon/Project4_Eyorokon.cpp
+++ Project4_Eyorokon/Project4_Eyorokon.cpp
@@ -6,17 +6,50 @@
 #include "HashTable.h"
 #include <string>
 
+/*Random number generator code for testing borrowed from Pilot*/
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
 using namespace std;
 
 int main()
 {
 	//HashTable<string> t;
 	HashTable<int> t;
-	int col=0, row;
-	cout << "first 12 "<< t.insert(12, 7777 , col) << endl; //Test insert.
-	cout <<"second 12 "<< t.insert(12, 7878, col) << endl;  //Test Duplicate entry
-	row = 12*12; //Quadratic insert
-	cout << "Key: " << t.table[row][0] << ". Value: " << t.data[row] << endl; //Print where it should be
+	
+	int col = 0;
+
+	//Random number generator code borrowed from pilot.
+	srand((unsigned int)time(NULL));
+	/* generate secret number between 1 and 10: */
+	
+	for (int i = 0; i < 1000; i++) {
+		int random = rand() % 1000 + 0;
+		cout << "Key: " << random << " inserted at: ";
+		t.insert(random, random/2, col);
+		cout << endl;
+	}
+	//cout << endl << t;
+	/*cout << endl << endl;
+	int val;
+	cout << t.find(666, val);
+	cout << "Value: for 666: " << val << endl;
+	cout << "Removed." << endl;
+	t.remove(666);
+	cout << t.find(666, val);*/
+	/*
+	int a = t.size;
+
+	for (int i = 0; i < 1000; i++) {
+		int random = rand() % 1000 + 0;
+		if(t.remove(random)) cout << "Key: " << random << " removed. ";
+		else (cout << "Key: " << random << " not found.");
+		cout << endl;
+	}
+	int b = t.size;
+	*/
+	//cout << t;
+	//cout << "Size before: " << a << ". Size after: " << b << endl;
 	system("pause");
     return 0;
 }
