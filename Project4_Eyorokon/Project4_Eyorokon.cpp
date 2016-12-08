acdcf1cd5c91f044cdd6768546e2516e84fd376d
veyorokon <veyor@DESKTOP-93D0BKC>
2016-12-08 18:12:41 -0500

    Finished. Improved probe hash.

--- Project4_Eyorokon/Project4_Eyorokon.cpp
+++ Project4_Eyorokon/Project4_Eyorokon.cpp
@@ -12,44 +12,67 @@
 
 using namespace std;
 
+void insert(HashTable<int>&);
 int main()
 {
 	//HashTable<string> t;
 	HashTable<int> t;
 	
-	int col = 0;
-
+	insert(t);
 	//Random number generator code borrowed from pilot.
-	srand((unsigned int)time(NULL));
+	
 	/* generate secret number between 1 and 10: */
 	
-	for (int i = 0; i < 1000; i++) {
-		int random = rand() % 1000 + 0;
-		cout << "Key: " << random << " inserted at: ";
-		t.insert(random, random/2, col);
-		cout << endl;
-	}
+	
 
 	//cout << endl << t;
-
+	/*
+	int col = 0;
 	for (int i = 0; i < 1000; i++) {
 		int random = rand() % 1000 + 0;
-		if(t.remove(random)) cout << "Key: " << random << " removed. ";
-		else (cout << "Key: " << random << " not found.");
-		cout << endl;
-	}
-
-
-	for (int i = 0; i < 1000; i++) {
-		int random = rand() % 1000 + 0;
-		cout << "Key: " << random << " inserted at: ";
+		//cout << "Key: " << random << " inserted. ";
 		t.insert(random, random / 2, col);
+		//cout << endl;
+	}
+	*/
+	for (int i = 0; i < 1000; i++) {
+	int random = rand() % 1000 + 0;
+	if(t.remove(random)) cout << ". Key: " << random << " removed. ";
+	else (cout << "Key: " << random << " not found.");
+	cout << endl;
+	}
+	
+	
+	int val;
+	for (int i = 0; i < 1000; i++) {
+		int random = rand() % 1000 + 0;
+		cout << "Key: " << random << " found. ";
+		t.find(random, val);
 		cout << endl;
 	}
-
+	//cout << t.size;
 	cout << t;
+	
 	
 	system("pause");
     return 0;
 }
 
+void insert(HashTable<int>& table) {
+	srand((unsigned int)time(NULL));
+	int col;
+	/*
+	while (table.size < 1000) {
+		int random = rand() % 1000 + 0;
+		//cout << "Key: " << random << " inserted. ";
+		table.insert(random, random / 2, col);
+		cout << table.size << endl;
+	}*/
+
+	for (int i = 0; i < 1000; i++) {
+		table.insert(i, 12, col);
+		cout << i << endl;
+	}
+	//cout << table;
+
+}
